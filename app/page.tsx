6c251896c0bcafac7e022af54bb0ca6f918c9d9a
ghun131 <hungdh131@gmail.com>
2024-07-01 11:58:22 +0700

    api fetch plans invocation

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,11 @@ async function getPlans(client: Api<unknown>): Promise<PlansPaginated> {
 }
 
 export default async function Home() {
+  const client: Api<unknown> = Client(process.env.NEXT_API_KEY as string, {
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
+  });
+
+  const planData = await getPlans(client);
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-800 to-gray-900 text-white p-8">
       <h1 className="text-4xl font-bold mb-8 text-center">
