cc73c6f9c87843c6510beccbff1d45bab96933ee
ghun131 <hungdh131@gmail.com>
2024-07-01 11:56:11 +0700

    add more code and turn Home to async

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
-import Image from "next/image";
+// import SubscriptionPlanList from "@/components/SubscriptionPlanList";
+import { Api, Client, PlansPaginated } from "lago-javascript-client";
 
-export default function Home() {
+async function getPlans(client: Api<unknown>): Promise<PlansPaginated> {
+  try {
+    const response = await client.plans.findAllPlans({ per_page: 10, page: 1 });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export default async function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-800 to-gray-900 text-white p-8">
       <h1 className="text-4xl font-bold mb-8 text-center">
